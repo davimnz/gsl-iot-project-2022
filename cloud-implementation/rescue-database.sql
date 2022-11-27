0ab2fe48c69aaedf4bc6ba41f74a839e74ff7860
laradelgado02 <117867945+laradelgado02@users.noreply.github.com>
2022-11-26 21:54:05 -0600

    Update rescue-database.sql

--- cloud-implementation/rescue-database.sql
+++ cloud-implementation/rescue-database.sql
@@ -1,4 +1,9 @@
 CREATE DATABASE rescue;
 USE rescue;
 
-CREATE TABLE messages
+CREATE TABLE messages(
+  nodeId INT PRIMARY KEY,
+  severity VARCHAR(255);
+  latitude DOUBLE(15,15),
+  longitude DOUBLE(15,15)
+ );
