7d02cf09cd56495d25f3c1d3a38098ffe652355b
laradelgado02 <117867945+laradelgado02@users.noreply.github.com>
2022-11-27 13:36:22 -0600

    Update mqtt_subscribe.py

--- cloud-implementation/mqtt_subscribe.py
+++ cloud-implementation/mqtt_subscribe.py
@@ -1,16 +1,13 @@
 import random
-
 import mysql.connector
 from paho.mqtt import client as mqtt_client
 from sshtunnel import SSHTunnelForwarder
 from mysql.connector import Error
 
-
 broker = '20.196.216.210'
 port = 1884
-topic = "srv/rescue"
+top_res = "srv/rescue"
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
-
 
 def connect_mqtt() -> mqtt_client:
     def on_connect(client, userdata, flags, rc):
@@ -23,15 +20,15 @@ def connect_mqtt() -> mqtt_client:
     client.connect(broker, port)
     return client
 
+
 def subscribe(client: mqtt_client, database):
     def on_message(client, userdata, msg):
         print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
-        add_message(database, msg.payload.decode())
-    client.subscribe(topic, qos=2)
+    client.subscribe(top_res)
     client.on_message = on_message
 
-    
-################################ SQL part starts here
+
+# SQL part stars here
 
 def connect_mysql(host, user, password, db):
     dbconn = mysql.connector.connect(
@@ -43,34 +40,32 @@ def connect_mysql(host, user, password, db):
     print('Connection to the database established')
     return dbconn
 
-  
+
 def disconnect_db(connection):
     if connection.is_connected():
         connection.close()
         print("MySQL connection is closed")
 
 
-        
-def add_message(database, message):
+def addMessage(connection, message):
     try:
-        cursor = database.cursor()
-        sql = "INSERT INTO messages (nodeId, severity, latitude, longitude) VALUES ('%s', '%s', '%s', '%s')"
-        #values = (int(listNode[0]), listNode[1], float(listNode[2]), float(listNode[3]))
-        values = (15, 'High', 19.3473711, -99.1303977) # Test to see if it inserts the values
-        cursor.execute(sql, values)
-        database.commit()
+        cursor = connection.cursor()
+        listmain = message.split(":")
+        sqlSTR = 'INSERT INTO messages (nodeId, severity, latitude, longitude) VALUES (%s, %s, %s, %s)'
+        values = (int(listmain[0]), listmain[1], listmain[2], listmain[3])
+        cursor.execute(sqlSTR, values)
+        connection.commit()
         print(cursor.rowcount, "Message added into the database")
-        
+        cursor.close()
     except mysql.connector.Error as error:
         print("Fail to add values".format(error))
-
-        
-########################################################### SQL part ends here
+# SQL part ends here
 
 def run():
-    rescue = connect_mysql('20.196.216.210', 'iotTest', 'SQL_iot_123', 'rescue')
+    dbconn = connect_mysql('20.196.216.210', 'iotTest', 'SQL_iot_123', 'rescue')
     client = connect_mqtt()
-    message = subscribe(client, rescue)
+    message = subscribe(client, dbconn)
+    addMessage(dbconn, message)
     client.loop_forever()
 
 run()
